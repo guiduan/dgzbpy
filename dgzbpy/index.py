52e324b3f25f500f0dcfeb92fb32c8921545c12e
guiduan <guiduan1@gmail.com>
2019-05-01 22:43:04 +0800

    add static dir

--- dgzbpy/index.py
+++ dgzbpy/index.py
@@ -2,4 +2,4 @@ from django.shortcuts import render_to_response
 
 
 def index(request):
-    return render_to_response('index.html', {'title': 'this a title', 'message': 'this is a message'})
+    return render_to_response('index.html', {'title': 'this is my front page title', 'message': 'this is my front page'})
